c3540e7431a5e63574d72a32d048e65abe37cede
TristanCavite <tristan.cavite@gmail.com>
2025-10-04 12:12:17 +0800

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,6 @@
-// eslint.config.mjs
-export default {
-  root: true,
-  env: { browser: true, node: true, es2021: true },
-  parserOptions: { ecmaVersion: 2021, sourceType: 'module' },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  overrides: [
-    {
-      files: ['pages/**/*.{vue,js,ts}', 'layouts/**/*.{vue,js,ts}'],
-      rules: { 'vue/multi-word-component-names': 'off' }
-    },
-    {
-      files: ['**/*.vue'],
-      rules: {
-        'vue/multi-word-component-names': ['error', { ignores: ['index','default','error','main'] }]
-      }
-    }
-  ],
-  rules: {}
-}
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  // Your custom configs here
+)
