18e1945eed3625d101053d08a9c22f418918f20b
TristanCavite <tristan.cavite@gmail.com>
2025-10-04 11:31:52 +0800

    fix exlint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,24 @@
-import withNuxt from './.nuxt/eslint.config.mjs'
-
-export default [
-  ...withNuxt,
-
-
-  {
-    files: ['pages/**/*.{vue,js,ts}', 'layouts/**/*.{vue,js,ts}'],
-    rules: {
-      'vue/multi-word-component-names': 'off',
+// eslint.config.mjs
+export default {
+  root: true,
+  env: { browser: true, node: true, es2021: true },
+  parserOptions: { ecmaVersion: 2021, sourceType: 'module' },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  overrides: [
+    {
+      files: ['pages/**/*.{vue,js,ts}', 'layouts/**/*.{vue,js,ts}'],
+      rules: { 'vue/multi-word-component-names': 'off' }
     },
-  },
-
-
-  {
-    files: ['*/.vue'],
-    rules: {
-      'vue/multi-word-component-names': ['error', {
-        ignores: ['index', 'default', 'error', 'main'],
-      }],
-    },
-  },
-]
+    {
+      files: ['**/*.vue'],
+      rules: {
+        'vue/multi-word-component-names': ['error', { ignores: ['index','default','error','main'] }]
+      }
+    }
+  ],
+  rules: {}
+}
